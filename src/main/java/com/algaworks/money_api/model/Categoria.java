fd85811314db4ca135c4c98d99ba624c30ca3384
GabrielFaria23 <gabrielfariapta01@gmail.com>
2021-03-16 17:09:57 -0300

    findAll findById and insert to categoria implemented

---
+++ src/main/java/com/algaworks/money_api/model/Categoria.java
@@ -0,0 +1,45 @@
+package com.algaworks.money_api.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name= "categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long codigo;
+
+    private String nome;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return codigo == categoria.codigo &&
+                Objects.equals(nome, categoria.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo, nome);
+    }
+}
