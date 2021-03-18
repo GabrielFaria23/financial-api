7bf273b9af9c86f70ca21f9e49d8340d09518ca3
GabrielFaria23 <gabrielfariapta01@gmail.com>
2021-03-18 09:54:22 -0300

    PessoaService created, update anda partial update implemented and make some exceptions

--- src/main/java/com/algaworks/money_api/resource/PessoaResource.java
+++ src/main/java/com/algaworks/money_api/resource/PessoaResource.java
@@ -3,16 +3,19 @@ package com.algaworks.money_api.resource;
 import com.algaworks.money_api.event.RecursoCriadoEvent;
 import com.algaworks.money_api.model.Pessoa;
 import com.algaworks.money_api.repository.PessoaRepository;
+import com.algaworks.money_api.service.PessoaService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -23,6 +26,9 @@ public class PessoaResource {
 
     @Autowired
     private ApplicationEventPublisher publisher;
+
+    @Autowired
+    private PessoaService pessoaService;
 
     @GetMapping
     public List<Pessoa> buscarTodasAsPessoas(){
@@ -43,5 +49,23 @@ public class PessoaResource {
         return pessoaRepository.findById(codigo)
                 .map(pessoa -> ResponseEntity.ok().body(pessoa))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{codigo}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remover(@PathVariable Long codigo) {
+        pessoaRepository.deleteById(codigo);
+    }
+
+    @PutMapping("/{codigo}")
+    public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa){
+        Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
+        return ResponseEntity.ok(pessoaSalva);
+    }
+
+    @PutMapping("/{codigo}/ativo")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizarPropriedadeAtivo(@PathVariable Long codigo,@Valid @RequestBody Boolean ativo){
+        pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
     }
 }
---
+++ src/main/java/com/algaworks/money_api/service/PessoaService.java
@@ -0,0 +1,39 @@
+package com.algaworks.money_api.service;
+
+import com.algaworks.money_api.model.Pessoa;
+import com.algaworks.money_api.repository.PessoaRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PessoaService {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+        Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+        BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+        return pessoaRepository.save(pessoaSalva);
+    }
+
+    public void atualizarPropriedadeAtivo(Long codigo, Boolean ativo) {
+        Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+        pessoaSalva.setAtivo(ativo);
+        pessoaRepository.save(pessoaSalva);
+    }
+
+    private Pessoa buscarPessoaPeloCodigo(Long codigo) {
+        Optional<Pessoa> pessoaOptional = pessoaRepository.findById(codigo);
+        Pessoa pessoaSalva = pessoaOptional.get();
+        if (pessoaSalva == null){
+            throw new EmptyResultDataAccessException(1);
+        }
+        return pessoaSalva;
+    }
+}
